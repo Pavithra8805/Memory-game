bce4273cbd5bc45a764eb715c0e49fd9a24dae24
Pavithra8805 <pavithra8805@gmail.com>
2023-12-14 19:45:29 +0000

    changed

--- script.js
+++ script.js
@@ -1,36 +1,105 @@
-const moves = document.getElementById("moves-count");
-const timeValue = document.getElementById("time");
 
-const gameContainer = document.querySelector(".game-container");
-const result = document.getElementById("result");
+document.getElementById("1").onclick = () => {
+    let questionMark = document.querySelectorAll(".mark")
 
-let firstCard = false;
-let secondCard = false;
-
-//Items array
-const images = [
-    "😇",
-    "🥳",
-    "😴",
-    "🤣",
-    "😎",
-    "😂",
-    "😜",
-    "🤩",
-];
-
-let timerDiv = document.getElementById("time");
-
-function countDown() {
-    localStorage.setItem("time", time);
-
-    if (time == 0) {
-        timerDiv.innerText = time;
-        time--;
-    } else {
-        window.location.href = "./gameover.html";
-    }
+    questionMark[0].style.display = "none"
 }
 
-setInterval(countDown, 1000);
+document.getElementById("2").onclick = () => {
+    let questionMark = document.querySelectorAll(".mark")
 
+    questionMark[1].style.display = "none"
+}
+
+document.getElementById("3").onclick = () => {
+    let questionMark = document.querySelectorAll(".mark")
+
+    questionMark[2].style.display = "none"
+}
+document.getElementById("4").onclick = () => {
+    let questionMark = document.querySelectorAll(".mark")
+
+    questionMark[3].style.display = "none"
+}
+document.getElementById("5").onclick = () => {
+    let questionMark = document.querySelectorAll(".mark")
+
+    questionMark[4].style.display = "none"
+}
+
+document.getElementById("6").onclick = () => {
+    let questionMark = document.querySelectorAll(".mark")
+
+    questionMark[5].style.display = "none"
+}
+document.getElementById("7").onclick = () => {
+    let questionMark = document.querySelectorAll(".mark")
+
+    questionMark[6].style.display = "none"
+}
+document.getElementById("8").onclick = () => {
+    let questionMark = document.querySelectorAll(".mark")
+
+    questionMark[7].style.display = "none"
+}
+
+document.getElementById("9").onclick = () => {
+    let questionMark = document.querySelectorAll(".mark")
+
+    questionMark[8].style.display = "none"
+}
+document.getElementById("10").onclick = () => {
+    let questionMark = document.querySelectorAll(".mark")
+
+    questionMark[9].style.display = "none"
+}
+document.getElementById("11").onclick = () => {
+    let questionMark = document.querySelectorAll(".mark")
+
+    questionMark[10].style.display = "none"
+}
+
+document.getElementById("12").onclick = () => {
+    let questionMark = document.querySelectorAll(".mark")
+
+    questionMark[11].style.display = "none"
+}
+document.getElementById("13").onclick = () => {
+    let questionMark = document.querySelectorAll(".mark")
+
+    questionMark[12].style.display = "none"
+}
+document.getElementById("14").onclick = () => {
+    let questionMark = document.querySelectorAll(".mark")
+
+    questionMark[13].style.display = "none"
+}
+
+document.getElementById("15").onclick = () => {
+    let questionMark = document.querySelectorAll(".mark")
+
+    questionMark[14].style.display = "none"
+}
+document.getElementById("16").onclick = () => {
+    let questionMark = document.querySelectorAll(".mark")
+
+    questionMark[15].style.display = "none"
+}
+
+
+// let movesCount = 0;
+// let gameStatus =    true;
+// if (name[0] === name[3]) {
+//     movesCount += 1;
+    
+// } else {
+//     movesCount = movesCount;
+//     questionMark[0].style.display = "block";
+// }
+// if (name[1] === name[7]) {
+//     movesCount += 1;
+//     questionMark[1].style.display = "none";
+// } else {
+//     movesCount == movesCount;
+//     questionMark[1].style.display = "block";
+// }
